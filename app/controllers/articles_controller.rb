e28366f5819e13f247505c6ba8bbbfa3621087a0
Ingrid <ingridhong@gmail.com>
2015-03-15 09:04:10 -0700

    new article works, merge does not

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -48,7 +48,7 @@ class ArticlesController < ContentController
 
   def merge
     begin 
-      @foreign = Article.find(params[:foreign_id])
+      @foreign = Article.find(params[:merge_with])
       @original = Article.find(params[:id])
       @original.update_attributes!(body: @original.body + @foreign.body)
       redirect_to admin_dashboard_path
